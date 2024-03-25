81de2fe4fb7ae9e592a7a1aa17ab51bd57d2a159
shraman <pkpaulpostdn@gmail.com>
2024-03-26 00:25:22 +0530

    added the 2nd component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,24 +10,27 @@ const NavigationMenuDemo: React.FC<NavigationMenuProps> = ({ items }) => {
 
   return (
     <div className="flex justify-center items-center">
-      <Image
-        src="/tpc_logo.svg"
-        alt="TPC Logo"
-        width={100}
-        height={24}
-        priority
-        className=""
-      />
-      <nav className="flex space-x-4 bg-custom-gray px-10 py-3 rounded-full">
-        {(items && items.length > 0 ? items : defaultItems).map((item) => (
-          <button
-            key={item}
-            className="px-3 py-2 bg-custom-gray-light hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 rounded-full"
-          >
-            {item}
-          </button>
-        ))}
-      </nav>
+      <div className="">
+        <Image
+          src="/tpc_logo.svg"
+          alt="TPC Logo"
+          width={100}
+          height={24}
+          priority
+        />
+      </div>
+      <div>
+        <nav className="flex space-x-4 bg-custom-gray px-10 py-3 rounded-full">
+          {(items && items.length > 0 ? items : defaultItems).map((item) => (
+            <button
+              key={item}
+              className="px-3 py-2 bg-custom-gray-light hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 rounded-full"
+            >
+              {item}
+            </button>
+          ))}
+        </nav>
+      </div>
     </div>
   );
 };
