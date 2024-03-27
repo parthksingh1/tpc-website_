51a98d98109f989e0cb7944d3b90f6c997b31900
shraman <pkpaulpostdn@gmail.com>
2024-03-28 03:54:10 +0530

    resolved rendereing  errors

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,20 @@ import Navbar from "./components/navbar";
 import SecondComponent from "./components/secondcomponent_home";
 import FourthComponent from "./components/fourthcomponent_home";
 
-
 export default function Home() {
   return (
     <div className="flex max-h-screen flex-col  bg-black text-white">
       <div className="navbar m-14">
         <Navbar items={[]} />
       </div>
-      <div><Firstcomponent/></div>
+      <div>
+        <Firstcomponent />
+      </div>
       <div className="flex ">
-        <SecondComponent/>
+        <SecondComponent />
       </div>
       <div>
-        <FourthComponent/> 
+        <FourthComponent />
       </div>
     </div>
   );
